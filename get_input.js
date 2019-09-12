a891bb83412640200955c2bb75322c7d3c3b0fad
Luis Carlin <luiscarlin@gmail.com>
2019-09-11 21:47:49 -0400

    do not json parse data

--- get_input.js
+++ get_input.js
@@ -1,26 +1,29 @@
-const axios = require('axios')
+const axios = require("axios");
 
-require('dotenv').config()
+require("dotenv").config();
 
 if (process.argv.length <= 2) {
   console.log("Usage: " + __filename + " day-number");
   process.exit(-1);
 }
 
-const cookie = process.env.COOKIE
-const year = process.env.YEAR
-const day = parseInt(process.argv[2])
+const cookie = process.env.COOKIE;
+const year = process.env.YEAR;
+const day = parseInt(process.argv[2]);
 
 if (!cookie || !year || !day) {
-  console.log('Error. Incorrect cookie, year, or day')
+  console.log("Error. Incorrect cookie, year, or day");
   process.exit(-1);
 }
 
-axios.get(`https://adventofcode.com/${year}/day/${day}/input`, { headers: { Cookie: `session=${cookie};`} })
+axios
+  .get(`https://adventofcode.com/${year}/day/${day}/input`, {
+    headers: { Cookie: `session=${cookie};` },
+    transformResponse: [data => data]
+  })
   .then(response => {
-    console.log(response.data)
+    console.log(response.data);
   })
   .catch(error => {
-    console.log(error)
-  }
-)
+    console.log(error);
+  });

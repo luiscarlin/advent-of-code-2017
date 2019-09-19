703fcf3d16b5310f983eb78f832ace1e1919562f
Luis Carlin <luiscarlin@gmail.com>
2019-09-18 21:41:05 -0400

    prep

--- 3.js
+++ 3.js
@@ -1,45 +1,6 @@
-const fs = require("fs")
+const num = 368078
 
-const main = () => {
-  const line = fs
-    .readFileSync("./1.in", "utf8")
-    .split("")
-    .map(n => Number(n))
-
-  console.log("part 1", firstPart(line))
-  console.log("part 2", secondPart(line))
-}
-
-const firstPart = line => {
-  let sum = 0
-
-  for (let i = 0; i < line.length; i++) {
-    let currentNum = line[i]
-    let nextNum = line[(i + 1) % line.length]
-
-    if (currentNum == nextNum) {
-      sum += line[i]
-    }
-  }
-
-  return sum
-}
-
-const secondPart = line => {
-  let half = line.length / 2
-  let sum = 0
-
-  for (let i = 0; i < line.length; i++) {
-    let currentNum = line[i]
-    let nextNum = line[(i + half) % line.length]
-
-    if (currentNum == nextNum) {
-      sum += line[i]
-    }
-  }
-
-  return sum
-}
+const main = () => {}
 
 if (require.main == module) {
   main()

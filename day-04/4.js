85faf65b847440c57c8c04a6f48bcafae21d18cd
Luis Carlin <luiscarlin@gmail.com>
2020-11-28 16:31:52 -0500

    day 4 part 2 done

--- day-04/4.js
+++ day-04/4.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
 
+const sotrString = (text) => text.split('').sort().join('')
+
 const passphrases = fs.readFileSync('./day-04/4.in', 'utf8').split('\n')
 
 let numberValid = 0
@@ -13,3 +15,17 @@ passphrases.forEach((passphrase) => {
 })
 
 console.log('part 1', numberValid)
+
+numberValid = 0
+
+passphrases.forEach((passphrase) => {
+  const words = passphrase.split(' ')
+
+  const sortedWords = words.map((word) => sotrString(word))
+
+  if (new Set(sortedWords).size === sortedWords.length) {
+    numberValid += 1
+  }
+})
+
+console.log('part 2', numberValid)
